04de1835ea060245088b5a6f17dcf8deacb8c3c7
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2023-05-24 19:55:25 +0200

    treci commit

--- src/views/JavascriptView.vue
+++ src/views/JavascriptView.vue
@@ -1,35 +1,45 @@
 <template>
   <div>
-    <BackgroundComponent/>
-    <DropdownMenu/>
+    <BackgroundComponent />
+    <DropdownMenu />
+    <v-container fill-height fluid class="background">
+      <BackgroundComponent />
+      <v-container>
+        <v-row align="center" justify="center" class="vrow">
+          <v-col class="text-center" cols="12">
+            <h1 class="text-h2 font-weight-thin mb-4">HTML Lectures</h1>
+            <h4 class="txth4">Learn how to use HTML!</h4>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
   </div>
 </template>
 <script>
-import BackgroundComponent from '@/components/BackgroundComponent.vue';
-import DropdownMenu from '@/components/DropdownMenu.vue'
+import BackgroundComponent from "@/components/BackgroundComponent.vue";
+import DropdownMenu from "@/components/DropdownMenu.vue";
 export default {
-    data() {
-        return {
-            lessons: [
-                {
-                    title: 'Home',
-                },
-                {
-                    title: 'Where to',
-                },
-                {
-                    title: 'Output',
-                },
-                {
-                    title: 'Statements',
-                },
-            ]
-
-        }
-    },
-    components: {
-        BackgroundComponent,
-        DropdownMenu,
-    }
-}
+  data() {
+    return {
+      lessons: [
+        {
+          title: "Home",
+        },
+        {
+          title: "Where to",
+        },
+        {
+          title: "Output",
+        },
+        {
+          title: "Statements",
+        },
+      ],
+    };
+  },
+  components: {
+    BackgroundComponent,
+    DropdownMenu,
+  },
+};
 </script>
