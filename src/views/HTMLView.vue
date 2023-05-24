97c605ada9262c47ccdb95843d40e2cbabb90b50
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2023-05-24 19:47:27 +0200

    drugi commit

---
+++ src/views/HTMLView.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container fill-height fluid class="background">
+      <BackgroundComponent />
+      <v-container>
+        <v-row align="center" justify="center" class="vrow">
+          <v-col class="text-center" cols="12">
+            <h1 class="text-h2 font-weight-thin mb-4">HTML Lectures</h1>
+            <h4 class="txth4">Learn how to use HTML!</h4>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+  </template>
+  
+  <style>
+  .txth4 {
+    font-size: 30px;
+    font-weight: 70;
+    color: #ffffff;
+  }
+  .vrow {
+    margin-bottom: 500px;
+  }
+  </style>
+  
+  <script>
+  import BackgroundComponent from "@/components/BackgroundComponent.vue";
+  export default {
+    components: {
+      BackgroundComponent,
+    },
+  };
+  </script>
--- src/views/JavascriptView.vue
+++ src/views/JavascriptView.vue
@@ -1,35 +1,42 @@
 <template>
-  <div>
-    <BackgroundComponent/>
-    <DropdownMenu/>
-  </div>
-</template>
-<script>
-import BackgroundComponent from '@/components/BackgroundComponent.vue';
-import DropdownMenu from '@/components/DropdownMenu.vue'
-export default {
+    <v-container>
+      <BackgroundComponent />
+      <DropdownMenu />
+      <v-container>
+        <v-row align="center" justify="center" class="vrow">
+          <v-col class="text-center" cols="12">
+            <h1 class="text-h2 font-weight-thin mb-4">CSS Lectures</h1>
+            <h4 class="txth4">Learn how to use CSS!</h4>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+  </template>
+  <script>
+  import BackgroundComponent from "@/components/BackgroundComponent.vue";
+  import DropdownMenu from "@/components/DropdownMenu.vue";
+  export default {
     data() {
-        return {
-            lessons: [
-                {
-                    title: 'Home',
-                },
-                {
-                    title: 'Where to',
-                },
-                {
-                    title: 'Output',
-                },
-                {
-                    title: 'Statements',
-                },
-            ]
-
-        }
+      return {
+        lessons: [
+          {
+            title: "Home",
+          },
+          {
+            title: "Where to",
+          },
+          {
+            title: "Output",
+          },
+          {
+            title: "Statements",
+          },
+        ],
+      };
     },
     components: {
-        BackgroundComponent,
-        DropdownMenu,
-    }
-}
-</script>
+      BackgroundComponent,
+      DropdownMenu,
+    },
+  };
+  </script>
