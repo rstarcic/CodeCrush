f73c16a87c9506bf3c4555286a2054d1ed1546fe
rstarcic <roberta.starcic589@gmail.com>
2023-06-03 21:40:46 +0200

    Update ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,21 +1,19 @@
 <template>
   <v-container fill-height fluid class="background">
     <BackgroundComponent />
-    <v-card class="profile">
+    <v-card class="profile-card">
       <v-navigation-drawer
         v-model="drawer"
-        :mini-variant.sync="mini"
+        mini-variant
+        mini-variant-width="55"
         permanent
-        width="300px"
       >
         <v-list-item class="px-2">
           <v-list-item-avatar>
-            <v-img :src="user.avatar"></v-img>
+            <v-img :src="avatar"></v-img>
           </v-list-item-avatar>
 
-          <v-list-item-title
-            >{{ user.firstName }} {{ user.lastName }}</v-list-item-title
-          >
+          <v-list-item-title>{{ firstName }} {{ lastName }}</v-list-item-title>
 
           <v-btn icon @click.stop="mini = !mini">
             <v-icon>mdi-chevron-left</v-icon>
@@ -26,10 +24,10 @@
 
         <v-list dense>
           <v-list-item
-            v-for="data in personalData"
+            v-for="data in settingsData"
             :key="data.title"
             link
-            @click="navigate(data.route)"
+            @click="setActiveSection(data.title)"
           >
             <v-list-item-icon>
               <v-icon>{{ data.icon }}</v-icon>
@@ -39,46 +37,28 @@
               <v-list-item-title>{{ data.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-
-          <v-divider></v-divider>
-
-          <v-list-item link @click="deleteAccount">
-            <v-list-item-icon>
-              <v-icon>mdi-delete</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>Brisanje računa</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
         </v-list>
       </v-navigation-drawer>
 
-      <v-card-text class="card-text">
-        <template v-if="activeRoute === '/personal'">
-          <h2>Uređivanje osobnih podataka</h2>
+      <v-card-text class="form-container">
+        <template v-if="activeSection === 'Personal Information'">
+          <h2>Edit your Personal Information</h2>
           <v-form>
-            <v-text-field v-model="user.firstName" label="Ime"></v-text-field>
+            <v-text-field v-model="firstName" label="First name"></v-text-field>
+            <v-text-field v-model="lastName" label="Last name"></v-text-field>
             <v-text-field
-              v-model="user.lastName"
-              label="Prezime"
-            ></v-text-field>
-            <v-text-field
-              v-model="user.email"
+              v-model="email"
               type="email"
               label="Email"
             ></v-text-field>
             <v-btn @click="saveChanges" color="primary">Save Changes</v-btn>
           </v-form>
         </template>
-        <template v-else-if="activeRoute === '/account'">
-          <h2>Postavke računa</h2>
-          <p>
-            Ovdje možete mijenjati svoje postavke računa, kao što su lozinka,
-            profilna slika i preferencije e-pošte.
-          </p>
+        <template v-else-if="activeSection === 'Account Settings'">
+          <h2>Edit Account Settings</h2>
+          <p>Here you can change your account settings,</p>
 
-          <v-form>
+          <v-form class="form">
             <v-text-field
               v-model="password"
               label="Password"
@@ -94,9 +74,29 @@
               label="Confirm Password"
               type="password"
             ></v-text-field>
-            <v-file-input v-model="avatar" label="Profile Image"></v-file-input>
+            <v-file-input
+              v-model="avatar"
+              type="file"
+              label="Profile Image"
+              accept="image/*"
+              @change="handleFileUpload"
+            ></v-file-input>
           </v-form>
-          <v-btn @click="saveChanges" color="primary">Save Changes</v-btn>
+          <v-btn @click="saveAccountSettings" color="primary"
+            >Save Changes</v-btn
+          >
+        </template>
+        <template v-if="activeSection === 'Delete Account'">
+          <v-card>
+            <v-card-title>Delete Account</v-card-title>
+            <v-card-text>
+              Are you sure you want to delete your account?
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="red" @click="deleteAccount">Yes</v-btn>
+              <v-btn text @click="cancelDeleteAccount">Cancel</v-btn>
+            </v-card-actions>
+          </v-card>
         </template>
       </v-card-text>
     </v-card>
@@ -112,23 +112,23 @@ export default {
     return {
       drawer: true,
       mini: false,
-
-      user: {
-        avatar: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-        uid: "",
-        password: "",
-        newPassword: "",
-        confirmPassword: "",
-        avatar: null,
-      },
-      personalData: [
-        { title: "Osobni podaci", icon: "mdi-account", route: "/personal" },
-        { title: "Postavke računa", icon: "mdi-cog", route: "/account" },
+      avatar: null,
+      firstName: "",
+      lastName: "",
+      email: "",
+      uid: "",
+      password: "",
+      newPassword: "",
+      confirmPassword: "",
+      settingsData: [
+        {
+          title: "Personal Information",
+          icon: "mdi-account",
+        },
+        { title: "Account Settings", icon: "mdi-cog" },
+        { title: "Delete Account", icon: "mdi-delete" },
       ],
-      activeRoute: null,
+      activeSection: null,
     };
   },
   mounted() {
@@ -138,9 +138,11 @@ export default {
     BackgroundComponent,
   },
   methods: {
-    navigate(route) {
-      this.activeRoute = route;
+    setActiveSection(title) {
+      this.activeSection = title;
+      this.drawer = false;
     },
+
     async fetchUserData() {
       try {
         const user = firebase.auth().currentUser;
@@ -150,10 +152,10 @@ export default {
           const userSnapshot = await db.collection("users").doc(userId).get();
           if (userSnapshot.exists) {
             const userData = userSnapshot.data();
-            this.user.avatar = userData.avatar;
-            this.user.firstName = userData.firstName;
-            this.user.lastName = userData.lastName;
-            this.user.email = userData.email;
+            this.avatar = userData.avatar;
+            this.firstName = userData.firstName;
+            this.lastName = userData.lastName;
+            this.email = userData.email;
           }
         }
       } catch (error) {
@@ -167,24 +169,47 @@ export default {
         const userRef = db.collection("users").doc(userId);
 
         const updatedData = {
-          firstName: this.user.firstName,
-          lastName: this.user.lastName,
-          email: this.user.email,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
         };
-
         try {
           await userRef.update(updatedData);
-          console.log("Document is successfully updated!");
+          console.log("Account settings saved successfully!");
         } catch (error) {
-          console.error("Error: ", error);
+          console.error("Error updating account settings:", error);
         }
       }
     },
     deleteAccount() {
-      // Ova metoda se poziva kada korisnik klikne na "Brisanje računa"
-      // Ovdje možete implementirati logiku za brisanje korisničkog računa i svih povezanih podataka
-      // Primjer: Pozvati API za brisanje korisničkog računa
-      // Nakon brisanja računa, možete izvršiti redirekciju na prijavu ili drugu odgovarajuću stranicu
+      const user = firebase.auth().currentUser;
+      if (user) {
+        user
+          .delete()
+          .then(() => {
+            console.log("Account deleted successfully!");
+            this.$router.push("/signup");
+          })
+          .catch((error) => {
+            console.error("Error deleting account:", error);
+          });
+      }
+    },
+    confirmDeleteAccount() {
+      this.activeRoute = "/delete";
+    },
+    cancelDeleteAccount() {
+      this.showDeleteConfirmation = false;
+    },
+    handleFileUpload(event) {
+      if (
+        event &&
+        event.target &&
+        event.target.files &&
+        event.target.files.length > 0
+      ) {
+        this.avatar = event.target.files[0];
+      }
     },
     async saveAccountSettings() {
       const user = firebase.auth().currentUser;
@@ -193,8 +218,6 @@ export default {
         const userRef = db.collection("users").doc(userId);
 
         const updatedData = {};
-
-        // Update the password if a new password is provided
         if (
           this.newPassword &&
           this.confirmPassword &&
@@ -211,14 +234,17 @@ export default {
           try {
             const storageRef = storage.ref();
             const imageRef = storageRef.child(`profile-images/${userId}`);
-            await imageRef.put(this.avatar);
-            const imageUrl = await imageRef.getDownloadURL();
+            const snapshot = await imageRef.put(this.avatar);
+            const imageUrl = await snapshot.ref.getDownloadURL();
             updatedData.avatar = imageUrl;
-            console.log("Profile image updated successfully!");
+            console.log("Profile image uploaded successfully!");
+            await userRef.update({ avatar: imageUrl });
+            console.log("Avatar URL updated in Firestore!");
           } catch (error) {
             console.error("Error updating profile image:", error);
           }
         }
+
         try {
           await userRef.update(updatedData);
           console.log("Account settings saved successfully!");
@@ -232,9 +258,24 @@ export default {
 </script>
 
 <style scoped>
-.card-text {
-  width: 300px;
-  height: 400px;
-  margin-bottom: 300px;
+.profile-card {
+  max-height: 600px;
+  min-width: 500px;
+  margin-bottom: 200px;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.form {
+  width: 100%;
+  max-width: 400px;
+}
+.delete-card {
+  margin-bottom: 30px;
 }
 </style>
